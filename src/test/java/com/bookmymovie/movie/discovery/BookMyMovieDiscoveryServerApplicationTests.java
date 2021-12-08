148526febb6b3a77c370b6f84345eed6f6eb177c
anilkamatham <75293771+anilkamatham@users.noreply.github.com>
2021-12-08 14:23:44 +0530

    Add files via upload

---
+++ src/test/java/com/bookmymovie/movie/discovery/BookMyMovieDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmymovie.movie.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyMovieDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
